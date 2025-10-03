5735dbc4e8bfaaff8e7e3539bf00919033f29b86
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-10-03 15:23:12 -0400

    bumped version #

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
  
 setup(
     name="jadnutils",
-    version="0.1.0", 
+    version="0.2.0", 
     packages=["jadnutils", "jadnutils.html", "jadnutils.utils", "jadnutils.support"],
     install_requires=[
         "pytest",
