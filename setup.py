947fd707fbcbf7c1aab3d7beeac5814439cb4ea9
Joshua Cohen <jcohen02@umd.edu>
2025-10-23 15:00:30 -0400

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
  
 setup(
     name="jadnutils",
-    version="0.1.0", 
-    packages=["jadnutils", "jadnutils.html", "jadnutils.utils", "jadnutils.support"],
+    version="0.2.0", 
+    packages=["jadnutils", "jadnutils.json", "jadnutils.html", "jadnutils.utils", "jadnutils.support"],
     install_requires=[
         "pytest",
         "pandas"
