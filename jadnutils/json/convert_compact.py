f5a70447e7d4d6b416e446189dfa6504a5117197
Joshua Cohen <jcohen02@umd.edu>
2025-10-23 11:43:33 -0400

    Raise Errors

--- jadnutils/json/convert_compact.py
+++ jadnutils/json/convert_compact.py
@@ -8,15 +8,15 @@ def convert_to_compact(json_obj) -> list:
         # Validate initial JSON Object
         valid = validate_json(json_obj)
         if not valid:
-            print("Error: Invalid JSON Object\n", json_obj)
+            raise ValueError("Invalid JSON Object", json_obj)
 
         compact_json = strip_keys(json_obj)
 
         # Validate Compact JSON Object
         valid_compact = validate_json(compact_json)
         if not valid_compact:
-            print("Error: Invalid Compact JSON Object\n", compact_json)
+            raise ValueError("Invalid Compact JSON Object", compact_json)
 
         return compact_json
     except Exception as e:
-        print(e)
+        raise ValueError("Error converting to compact JSON: " + str(e))
