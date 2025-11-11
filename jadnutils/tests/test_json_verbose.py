5f07f4961443e6f2ced0377262affe580b133525
Joshua Cohen <jcohen02@umd.edu>
2025-11-11 11:47:53 -0500

    More compact to verbose logic

--- jadnutils/tests/test_json_verbose.py
+++ jadnutils/tests/test_json_verbose.py
@@ -2,7 +2,8 @@ import os
 import sys
 import json
 
-from jadnutils.utils.rev_conversion_utils import compact_to_verbose
+from jadnutils.utils.rev_conversion_utils import compact_to_verbose, get_real_type_order
+from jadnutils.utils.rev_conversion_utils import get_jadn_type_by_name
 sys.path.append(os.path.join(os.path.dirname(__file__), "test_data"))
 from music_lib_compact_data import j_data
 from music_lib import j_schema
@@ -21,8 +22,41 @@ def write_verbose_output(verbose_output, filename):
         f.write(json.dumps(verbose_output))
     print(verbose_output)
 
+def test_get_real_type_order():
+    jadn_schema = {
+        "meta": {"roots": ["TypeB"]},
+        "types": [
+            ["TypeA", "Record", [], "", [
+                [1, "field1", "TypeC", [], ""]
+            ]],
+            ["TypeB", "Record", [], "", [
+                [1, "subfield1", "TypeA", [], ""],
+            ]],
+            ["TypeC", "Map", [], "", [
+                [1, "mapfield1", "String", [], ""]
+            ]]
+        ]
+    }
+    jadn_types = jadn_schema.get('types', {})
+    root_name = jadn_schema.get('meta', {}).get('roots', [None])[0]
+    root_def = get_jadn_type_by_name(jadn_types, root_name)
+    
+    expected_order = [
+        ["TypeB", "Record", [], "", [
+            [1, "subfield1", "TypeA", [], ""],
+        ]],
+        ["TypeA", "Record", [], "", [
+            [1, "field1", "TypeC", [], ""]
+        ]],
+        ["TypeC", "Map", [], "", [
+            [1, "mapfield1", "String", [], ""]
+        ]]
+    ]
+    assert get_real_type_order(jadn_types, [], root_def) == expected_order
+
 def test_compact_to_verbose():
     jadn_schema = {
+        "meta": {"roots": ["Record-Name"]},
         "types": [
             ["Record-Name", "Record", [], "", [
                 [1, "string_field", "String", [], ""],
@@ -31,6 +65,9 @@ def test_compact_to_verbose():
         ]
     }
     jadn_types = jadn_schema.get('types', {})
+    root_name = jadn_schema.get('meta', {}).get('roots', [None])[0]
+    root_def = get_jadn_type_by_name(jadn_types, root_name)
+    ordered_types = get_real_type_order(jadn_types, [], root_def)
     
     nested_json = ["test", 1]
 
@@ -39,10 +76,11 @@ def test_compact_to_verbose():
         "int_field": 1
     }
 
-    assert compact_to_verbose(jadn_types, nested_json) == expected_json
+    assert compact_to_verbose(ordered_types, nested_json, root_def) == expected_json
 
 def test_compact_to_verbose_complex():
     jadn_schema = {
+        "meta": {"roots": ["Map-Name"]},
         "types": [
             ["Map-Name", "Map", [], "", [
                 [1, "key_field", "String", [], ""],
@@ -55,6 +93,9 @@ def test_compact_to_verbose_complex():
         ]
     }
     jadn_types = jadn_schema.get('types', {})
+    root_name = jadn_schema.get('meta', {}).get('roots', [None])[0]
+    root_def = get_jadn_type_by_name(jadn_types, root_name)
+    ordered_types = get_real_type_order(jadn_types, [], root_def)
 
     nested_json = {
         "key_field": "test",
@@ -69,10 +110,11 @@ def test_compact_to_verbose_complex():
         }
     }
 
-    assert compact_to_verbose(jadn_types, nested_json) == expected_json
+    assert compact_to_verbose(ordered_types, nested_json, root_def) == expected_json
 
 def test_compact_to_verbose_dupe_children():
     jadn_schema = {
+        "meta": {"roots": ["Map-Name"]},
         "types": [
             ["Map-Name", "Map", [], "", [
                 [1, "key_field", "String", [], ""],
@@ -90,6 +132,9 @@ def test_compact_to_verbose_dupe_children():
         ]
     }
     jadn_types = jadn_schema.get('types', {})
+    root_name = jadn_schema.get('meta', {}).get('roots', [None])[0]
+    root_def = get_jadn_type_by_name(jadn_types, root_name)
+    ordered_types = get_real_type_order(jadn_types, [], root_def)
 
     nested_json = {
         "key_field": "test",
@@ -109,7 +154,7 @@ def test_compact_to_verbose_dupe_children():
         }
     }
 
-    assert compact_to_verbose(jadn_types, nested_json) == expected_json
+    assert compact_to_verbose(ordered_types, nested_json, root_def) == expected_json
 
 def test_compact_to_verbose_reverse():
     jadn_schema = {
--- jadnutils/utils/rev_conversion_utils.py
+++ jadnutils/utils/rev_conversion_utils.py
@@ -1,27 +1,99 @@
-from jadnutils.utils.jadn_utils import get_field_by_data, get_type, get_field_from_struct, get_children, get_options, get_true_type_def
+from jadnutils.utils.jadn_utils import get_field_by_data, get_type, get_field_from_struct, get_children, get_options, get_true_type_def, get_parent
+from jadnutils.utils.consts import CORE_TYPES, PRIMITIVE_TYPES
+# def compact_to_verbose(jadn_types, json_obj):
+#     """
+#     Convert compact JSON object to verbose representation.
+#     Ex: [1, 2] => {"a": 1, "b": 2}
+#     """
+#     if isinstance(json_obj, list):
+#         field = get_field_from_compact_data(jadn_types, json_obj)
+#         type = get_type(field)
+#         if type == "Record":
+#             children = get_children(field)
+#             result = {}
+#             for i, child in enumerate(children):
+#                 key = child[1]
+#                 value = json_obj[i] if i < len(json_obj) else None
+#                 result[key] = compact_to_verbose(jadn_types, value)
+#             return result
+#         else:
+#             return [compact_to_verbose(jadn_types, value) for value in json_obj]
+#     elif isinstance(json_obj, dict):
+#         return {key: compact_to_verbose(jadn_types, value) for key, value in json_obj.items()}
+#     else:
+#         return json_obj
 
-def compact_to_verbose(jadn_types, json_obj):
+def compact_to_verbose(jadn_types, json_obj, type_def):
     """
-    Convert compact JSON object to verbose representation.
-    Ex: [1, 2] => {"a": 1, "b": 2}
+    Convert compact JSON object to verbose representation using the JADN Schema.
+    Call and store get_real_type_order in jadn_types before passing in jadn_types
     """
+
+    if not type_def:
+        return json_obj
+
+    next_type_def = jadn_types[1] if len(jadn_types) > 1 else None
+    next_jadn_types = jadn_types[1:] if len(jadn_types) > 1 else []
+
+    if isinstance(json_obj, dict):
+        return {key: compact_to_verbose(next_jadn_types, value, next_type_def) for key, value in json_obj.items()}
     if isinstance(json_obj, list):
-        field = get_field_from_compact_data(jadn_types, json_obj)
-        type = get_type(field)
-        if type == "Record":
-            children = get_children(field)
+        curr_type = get_type(type_def)
+        if curr_type == "Record":
+            children = get_children(type_def)
             result = {}
             for i, child in enumerate(children):
+                # See if parent matches
+                parent_field = get_parent(jadn_types, child)
+                if parent_field and parent_field[0] != type_def[0]:
+                    type_def = parent_field
                 key = child[1]
                 value = json_obj[i] if i < len(json_obj) else None
-                result[key] = compact_to_verbose(jadn_types, value)
+                result[key] = compact_to_verbose(jadn_types, value, type_def)
             return result
         else:
-            return [compact_to_verbose(jadn_types, value) for value in json_obj]
-    elif isinstance(json_obj, dict):
-        return {key: compact_to_verbose(jadn_types, value) for key, value in json_obj.items()}
+            return [compact_to_verbose(next_jadn_types, value, next_type_def) for value in json_obj]
     else:
         return json_obj
+
+def get_real_type_order(jadn_types, visited, type_def):
+    """
+    Returns a flat list of type definitions in the order they are encountered,
+    starting from the root type.
+    """
+    if type_def[0] in visited:
+        return []
+    visited.append(type_def[0])
+    result = [type_def]
+    for field in get_children(type_def):
+        child_type_name = get_type(field)
+        child_type_def = get_jadn_type_by_name(jadn_types, child_type_name)
+        if child_type_def:
+            result += get_real_type_order(jadn_types, visited, child_type_def)
+    return result
+
+def make_jadn_frequency_map(jadn_types):
+    """
+    Create a frequency map of JADN types used in the schema.
+    """
+    freq_map = {}
+    for jadn_type in jadn_types:
+        type_name = jadn_type[0]
+        freq_map[type_name] = freq_map.get(type_name, 0) + 1
+    return freq_map
+
+def get_jadn_type_by_name(jadn_types, name):
+    """
+    Retrieve a JADN Type by providing a type name
+    """
+    if not name or not jadn_types:
+        return None
+
+    for jadn_type in jadn_types:
+        if jadn_type[0] == name:
+            return jadn_type
+
+    return None
 
 def get_field_from_compact_data(jadn_types, data):
     """
