7fbf884ea68e101716fa1b36e139d7dcb8f217d6
Joshua Cohen <jcohen02@umd.edu>
2025-11-13 13:59:25 -0500

    Binary type tweak

--- jadnutils/utils/rev_conversion_utils.py
+++ jadnutils/utils/rev_conversion_utils.py
@@ -255,7 +255,7 @@ def valid_children_length(jadn_types, type_def, json_obj):
     """
     Check if number of children in type def matches numbers of keys in json_obj
     """
-    if not type_def or not json_obj:
+    if type_def is None or json_obj is None:
         return False
     
     try:
@@ -353,7 +353,7 @@ def get_python_type(jadn_types, field, id = False, direct_type = None, value = N
         "Integer": int,
         "Number": float,
         "Boolean": bool,
-        "Binary": bytes,
+        "Binary": str,
         "Record": list, # Compact changes records to lists
         "Enumerated": int if id else str,
         "Choice": dict,
